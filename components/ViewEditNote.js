eb0fa5ad6aea081dd54bf61faeac620bbe9957b6
Brian Quiroz <r229q057@ku.edu>
2019-11-18 16:19:48 -0600

    Add view/edit note button component

---
+++ components/ViewEditNote.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Alert, Button, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export function ViewEditNote(props) {
+  return (
+    <View style={styles.viewEditNoteContainer}>
+
+      <TouchableOpacity
+          style={styles.viewEditNoteButton}
+          activeOpacity = { .5 }
+          onPress={ () => Alert.alert('Simple Button pressed') } //Temporal, change later
+       >
+
+            <Text style={styles.viewEditNoteText}> View/Edit Today's Note </Text>
+
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewEditNoteContainer: {
+    paddingHorizontal: 15,
+  },
+  viewEditNoteButton: {
+    backgroundColor: "#1ac937",
+    borderRadius: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 5,
+      },
+    })
+  },
+  viewEditNoteText: {
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "white",
+    paddingVertical: 10
+  }
+});
